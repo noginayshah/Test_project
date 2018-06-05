6783338b4438b24e63e707c908f984ae12a13554
noginayshah <sushko@ukr.net>
2018-06-05 13:02:03 +0300

    cube

--- cube.py
+++ cube.py
@@ -2,13 +2,13 @@ from random import randint
 
 x = 6
 y = 6
-def cube ():
-    n=1
+def cube():
+    n = 1
     while True:
-        x = randint (1, 6)
-        y = randint (1, 6)
-        if x + y == 8 :
-            print (x, y, n)
+        x = randint(1, 6)
+        y = randint(1, 6)
+        if x + y == 8:
+            print(x, y, n)
             break
         n += 1
 cube()
